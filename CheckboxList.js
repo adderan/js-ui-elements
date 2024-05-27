355a2896acf8ec54778588facbf0decddd1a5639
Alden Deran <aldenderan@protonmail.com>
2024-05-27 13:05:56 -0700

    Make checkboxlist an Element

--- CheckboxList.js
+++ CheckboxList.js
@@ -1,42 +1,100 @@
-export class CheckBoxList {
-    constructor(root_div, label_text, update_callback, default_checked=false) {
-        this.root_div = root_div;
-        this.root_div.setAttribute('class', 'checkbox-list');
-        this.root_div.setAttribute('tabindex', '100');
+export class CheckBoxList extends HTMLElement {
+    constructor() {
+        super();
+        this.label_text = this.getAttribute('label-text');
+        this.default_checked = this.getAttribute('default-checked');
+    }
+    
+    connectedCallback() {
+        //this.setAttribute('tabindex', 100);
 
         this.list = document.createElement('ul');
         this.list.setAttribute('class', 'items');
 
         this.anchor = document.createElement('span');
         this.anchor.setAttribute('class', 'anchor');
-        this.anchor.innerHTML = label_text;
+        this.anchor.textContent = this.label_text;
 
-        this.root_div.appendChild(this.anchor);
-        this.root_div.appendChild(this.list);
+        this.appendChild(this.anchor);
+        this.appendChild(this.list);
 
-        //this.anchor.addEventListener('click', this);
+        this.addEventListener('focusout', this);
         document.addEventListener('click', this);
+
         this.visible = false;
 
         this.checkboxes = {};
-        this.update_callback = update_callback;
-        this.default_checked = default_checked;
+
+        this.style.display = 'inline-block';
+
+        const style = document.createElement('style');
+        style.innerHTML = `
+            .anchor {
+                position: relative;
+                cursor: pointer;
+                display: inline-block;
+                padding: 5px 50px 5px 10px;
+                border: 1px solid #ccc;
+            }
+
+            .anchor:after {
+                position: absolute;
+                content: "";
+                border-left: 2px solid black;
+                border-top: 2px solid black;
+                padding: 5px;
+                right: 10px;
+                top: 20%;
+                -moz-transform: rotate(-135deg);
+                -ms-transform: rotate(-135deg);
+                -o-transform: rotate(-135deg);
+                -webkit-transform: rotate(-135deg);
+                transform: rotate(-135deg);
+            }
+
+            .anchor:active:after {
+                right: 8px;
+                top: 21%;
+            }
+
+            ul.items {
+                padding: 2px;
+                display: none;
+                margin: 0;
+                border: 1px solid #ccc;
+                border-top: none;
+                position: fixed;
+                background-color: white;
+            }
+
+            ul.items li {
+                list-style: none;
+            }
+            ul.items li span {
+                margin-left: 4px;
+            }
+
+            .visible .anchor {
+                color: #0094ff;
+            }
+
+            .visible .items {
+                display: block;
+            }
+        `;
+
+        this.appendChild(style);
 
     }
 
-    dispatchEvent(event) {
-        for (let [item_name, checkbox] of Object.entries(this.checkboxes)) {
-            checkbox.dispatchEvent(event);
-        }
-    }
 
     toggle_visible() {
         if (this.visible) {
-            this.root_div.classList.remove('visible');
+            this.classList.remove('visible');
             this.visible = false;
         }
         else {
-            this.root_div.classList.add('visible');
+            this.classList.add('visible');
             this.visible = true;
         }
     }
@@ -53,14 +111,13 @@ export class CheckBoxList {
     handleEvent(evt) {
         if (evt.type == "click") {
             if (evt.target == this.anchor) {
-                console.log("anchor clicked");
                 this.toggle_visible();
             }
             else if (this.visible && !this.list.contains(evt.target)) {
                 //click outside the list, so hide
-                this.root_div.classList.remove('visible');
-                this.visible = false;
+                this.toggle_visible();
             }
+
         }
     }
     has_item(name) {
@@ -73,13 +130,11 @@ export class CheckBoxList {
         let list_item = document.createElement('li');
         let checkbox = document.createElement('input');
         checkbox.setAttribute('type', 'checkbox');
-        checkbox.onchange = this.update_callback;
 
         if (this.default_checked) {
             checkbox.checked = true;
         }
         let label = document.createElement('span');
-        label.setAttribute('style', 'margin-left:4px');
         label.innerHTML = name;
         list_item.appendChild(checkbox);
         list_item.appendChild(label);
